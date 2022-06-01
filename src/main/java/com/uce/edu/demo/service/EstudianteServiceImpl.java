9aacfdd8e9490d73509e6b88ee33a50e961710ba
Sebas0399 <antony.aguay@gmail.com>
2022-05-31 19:54:02 -0500

    Estereotipos service y repository

---
+++ src/main/java/com/uce/edu/demo/service/EstudianteServiceImpl.java
@@ -0,0 +1,33 @@
+package com.uce.edu.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.uce.edu.demo.modelo.Estudiante;
+@Service
+public class EstudianteServiceImpl implements IEstudianteService{
+
+	@Override
+	public void ingrersarEstudiante(Estudiante e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Estudiante buscarPorApellido(String apellido) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void actualizarEstudiante(Estudiante e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminarEstudiante(String cedula) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
